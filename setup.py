472d40a932013802584c07abb2be380abca53b78
Eduardo A. Paris Penas <edward2a@gmail.com>
2018-06-05 23:24:09 +0100

    version bump; install with py_modules

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='uconfload',
-    version='0.1',
+    version='0.1.3',
     description='Universal config loader for standalone and containerized apps.',
     url='https://github.com/edward2a/uconfload-python',
     author='Eduardo A. Paris Penas',
     author_email='edward2a@gmail.com, epp@realread.me',
     license='Apache License 2.0',
-    packages=['uconfload'],
+    py_modules=['uconfload'],
     install_requires=[
         'PyYAML'
     ],
